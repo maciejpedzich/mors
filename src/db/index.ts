b0d00ef4b06e5099ed6236aaea6c2510bd5cf20c
Maciej Pędzich <contact@maciejpedzi.ch>
2023-07-18 00:21:23 +0200

    Remove DrizzleORM's migrate call

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,4 @@
 import { drizzle } from 'drizzle-orm/mysql2';
-import { migrate } from 'drizzle-orm/mysql2/migrator';
 import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection(
@@ -7,5 +6,3 @@ const connection = await mysql.createConnection(
 );
 
 export const db = drizzle(connection);
-
-await migrate(db, { migrationsFolder: '../../drizzle' });
