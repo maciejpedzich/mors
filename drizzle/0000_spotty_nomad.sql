ea672a9ac033d5efb8815f7196e5ad39e52e0e53
Maciej Pędzich <contact@maciejpedzi.ch>
2023-07-19 02:45:17 +0200

    Add autoincrement to all id fields

--- drizzle/0000_spotty_nomad.sql
+++ drizzle/0000_spotty_nomad.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `drivers` (
-	`id` smallint PRIMARY KEY NOT NULL,
+	`id` smallint AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`team_id` smallint,
 	`first_name` varchar(10),
 	`last_name` varchar(10),
@@ -8,19 +8,20 @@ CREATE TABLE `drivers` (
 );
 --> statement-breakpoint
 CREATE TABLE `incidents` (
-	`id` smallint PRIMARY KEY NOT NULL,
+	`id` smallint AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`session_id` smallint,
 	`driver_id` smallint,
 	`date_noted` datetime,
+	`lap_number` smallint,
 	`infringement` smallint,
 	`penalty` smallint
 );
 --> statement-breakpoint
 CREATE TABLE `laps` (
-	`id` smallint PRIMARY KEY NOT NULL,
+	`id` smallint AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`driver_id` smallint,
 	`session_id` smallint,
-	`tyre_compund` enum('s','m','h','i','w'),
+	`tyre_compound` enum('s','m','h','i','w'),
 	`sector1_time_ms` mediumint,
 	`sector2_time_ms` mediumint,
 	`sector3_time_ms` mediumint,
@@ -29,24 +30,30 @@ CREATE TABLE `laps` (
 );
 --> statement-breakpoint
 CREATE TABLE `results` (
-	`id` smallint PRIMARY KEY NOT NULL,
+	`id` smallint AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`session_id` smallint,
 	`driver_id` smallint,
 	`status` enum('fin','dnf','dsq','ret'),
 	`position` smallint,
-	`points` smallint
+	`points` smallint,
+	`penalties_seconds` smallint
 );
 --> statement-breakpoint
 CREATE TABLE `rounds` (
-	`id` smallint PRIMARY KEY NOT NULL,
-	`round_id` smallint,
+	`id` smallint AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`name` varchar(15),
+	`country_code` varchar(5)
+);
+--> statement-breakpoint
+CREATE TABLE `sessions` (
+	`id` smallint AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	`round_id` smallint,
 	`type` enum('p','q','r'),
 	`date_started` datetime,
 	`date_finished` datetime
 );
 --> statement-breakpoint
 CREATE TABLE `teams` (
-	`id` smallint PRIMARY KEY NOT NULL,
+	`id` smallint AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`name` varchar(20)
 );
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -12,27 +12,26 @@ import {
 } from 'drizzle-orm/mysql-core';
 
 export const rounds = mysqlTable('rounds', {
-  id: smallint('id').primaryKey(),
+  id: smallint('id').primaryKey().autoincrement(),
   name: varchar('name', { length: 15 }),
   countryCode: varchar('country_code', { length: 5 })
 });
 
-export const sessions = mysqlTable('rounds', {
-  id: smallint('id').primaryKey(),
+export const sessions = mysqlTable('sessions', {
+  id: smallint('id').primaryKey().autoincrement(),
   roundId: smallint('round_id'),
-  name: varchar('name', { length: 15 }),
   type: mysqlEnum('type', ['p', 'q', 'r']),
   dateStarted: datetime('date_started'),
   dateFinished: datetime('date_finished')
 });
 
 export const teams = mysqlTable('teams', {
-  id: smallint('id').primaryKey(),
+  id: smallint('id').primaryKey().autoincrement(),
   name: varchar('name', { length: 20 })
 });
 
 export const drivers = mysqlTable('drivers', {
-  id: smallint('id').primaryKey(),
+  id: smallint('id').primaryKey().autoincrement(),
   teamId: smallint('team_id'),
   firstName: varchar('first_name', { length: 10 }),
   lastName: varchar('last_name', { length: 10 }),
@@ -41,10 +40,10 @@ export const drivers = mysqlTable('drivers', {
 });
 
 export const laps = mysqlTable('laps', {
-  id: smallint('id').primaryKey(),
+  id: smallint('id').primaryKey().autoincrement(),
   driverId: smallint('driver_id'),
   sessionId: smallint('session_id'),
-  tyreCompund: mysqlEnum('tyre_compund', ['s', 'm', 'h', 'i', 'w']),
+  tyreCompound: mysqlEnum('tyre_compound', ['s', 'm', 'h', 'i', 'w']),
   sector1TimeMs: mediumint('sector1_time_ms'),
   sector2TimeMs: mediumint('sector2_time_ms'),
   sector3TimeMs: mediumint('sector3_time_ms'),
@@ -53,21 +52,23 @@ export const laps = mysqlTable('laps', {
 });
 
 export const incidents = mysqlTable('incidents', {
-  id: smallint('id').primaryKey(),
+  id: smallint('id').primaryKey().autoincrement(),
   sessionId: smallint('session_id'),
   driverId: smallint('driver_id'),
   dateNoted: datetime('date_noted'),
+  lapNumber: smallint('lap_number'),
   infringement: smallint('infringement'),
   penalty: smallint('penalty')
 });
 
 export const results = mysqlTable('results', {
-  id: smallint('id').primaryKey(),
+  id: smallint('id').primaryKey().autoincrement(),
   sessionId: smallint('session_id'),
   driverId: smallint('driver_id'),
   status: mysqlEnum('status', ['fin', 'dnf', 'dsq', 'ret']),
   position: smallint('position'),
-  points: smallint('points')
+  points: smallint('points'),
+  penaltiesSeconds: smallint('penalties_seconds')
 });
 
 export const roundsRelations = relations(rounds, ({ many }) => ({
