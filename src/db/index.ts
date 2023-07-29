59ff057e6027cc10ec1a5a4da72fe0cd4048724f
Maciej Pędzich <contact@maciejpedzi.ch>
2023-07-29 21:36:53 +0200

    Remove redundant string type inferrence

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,6 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 
-const connection = await mysql.createConnection(
-  process.env.DATABASE_URL as string
-);
+const connection = await mysql.createConnection(import.meta.env.DATABASE_URL);
 
-export const db = drizzle(connection);
+export const db = drizzle(connection, { logger: import.meta.env.DEV });
