e42780505bc8910a1be236886441eefe261142a4
Maciej Pędzich <contact@maciejpedzi.ch>
2023-08-01 16:00:03 +0200

    Add notNull to all table fields

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -12,123 +12,62 @@ import {
 } from 'drizzle-orm/mysql-core';
 
 export const rounds = mysqlTable('rounds', {
-  id: smallint('id').primaryKey().autoincrement(),
-  name: varchar('name', { length: 15 }),
-  countryCode: varchar('country_code', { length: 5 })
+  id: smallint('id').primaryKey().autoincrement().notNull(),
+  name: varchar('name', { length: 15 }).notNull(),
+  countryCode: varchar('country_code', { length: 5 }).notNull()
 });
 
 export const sessions = mysqlTable('sessions', {
-  id: smallint('id').primaryKey().autoincrement(),
-  roundId: smallint('round_id'),
-  type: mysqlEnum('type', ['p', 'q', 'r']),
-  dateStarted: datetime('date_started'),
-  dateFinished: datetime('date_finished')
+  id: smallint('id').primaryKey().autoincrement().notNull(),
+  roundId: smallint('round_id').notNull(),
+  type: mysqlEnum('type', ['p', 'q', 'r']).notNull(),
+  dateStarted: datetime('date_started').notNull(),
+  dateFinished: datetime('date_finished').notNull()
 });
 
 export const teams = mysqlTable('teams', {
-  id: smallint('id').primaryKey().autoincrement(),
-  name: varchar('name', { length: 20 })
+  id: smallint('id').primaryKey().autoincrement().notNull(),
+  name: varchar('name', { length: 20 }).notNull()
 });
 
 export const drivers = mysqlTable('drivers', {
-  id: smallint('id').primaryKey().autoincrement(),
-  teamId: smallint('team_id'),
-  firstName: varchar('first_name', { length: 10 }),
-  lastName: varchar('last_name', { length: 10 }),
-  countryCode: char('country_code', { length: 2 }),
-  human: boolean('human')
+  id: smallint('id').primaryKey().autoincrement().notNull(),
+  teamId: smallint('team_id').notNull(),
+  firstName: varchar('first_name', { length: 10 }).notNull(),
+  lastName: varchar('last_name', { length: 10 }).notNull(),
+  countryCode: char('country_code', { length: 2 }).notNull(),
+  human: boolean('human').notNull()
 });
 
 export const laps = mysqlTable('laps', {
-  id: smallint('id').primaryKey().autoincrement(),
-  driverId: smallint('driver_id'),
-  sessionId: smallint('session_id'),
-  lapNumber: smallint('lap_number'),
-  tyreCompound: mysqlEnum('tyre_compound', ['s', 'm', 'h', 'i', 'w']),
-  sector1TimeMs: mediumint('sector1_time_ms'),
-  sector2TimeMs: mediumint('sector2_time_ms'),
-  sector3TimeMs: mediumint('sector3_time_ms'),
-  totalTimeMs: mediumint('total_time_ms'),
-  valid: boolean('valid')
+  id: smallint('id').primaryKey().autoincrement().notNull(),
+  driverId: smallint('driver_id').notNull(),
+  sessionId: smallint('session_id').notNull(),
+  lapNumber: smallint('lap_number').notNull(),
+  tyreCompound: mysqlEnum('tyre_compound', ['s', 'm', 'h', 'i', 'w']).notNull(),
+  sector1TimeMs: mediumint('sector1_time_ms').notNull(),
+  sector2TimeMs: mediumint('sector2_time_ms').notNull(),
+  sector3TimeMs: mediumint('sector3_time_ms').notNull(),
+  totalTimeMs: mediumint('total_time_ms').notNull(),
+  valid: boolean('valid').notNull()
 });
 
 export const incidents = mysqlTable('incidents', {
-  id: smallint('id').primaryKey().autoincrement(),
-  sessionId: smallint('session_id'),
-  driverId: smallint('driver_id'),
-  dateNoted: datetime('date_noted'),
-  lapNumber: smallint('lap_number'),
-  infringement: smallint('infringement'),
-  penalty: smallint('penalty')
+  id: smallint('id').primaryKey().autoincrement().notNull(),
+  sessionId: smallint('session_id').notNull(),
+  driverId: smallint('driver_id').notNull(),
+  dateNoted: datetime('date_noted').notNull(),
+  lapNumber: smallint('lap_number').notNull(),
+  infringement: smallint('infringement').notNull(),
+  penalty: smallint('penalty').notNull()
 });
 
 export const results = mysqlTable('results', {
-  id: smallint('id').primaryKey().autoincrement(),
-  sessionId: smallint('session_id'),
-  driverId: smallint('driver_id'),
-  status: mysqlEnum('status', ['fin', 'dnf', 'dsq', 'ret']),
-  position: smallint('position'),
-  points: smallint('points'),
-  penaltiesSeconds: smallint('penalties_seconds')
+  id: smallint('id').primaryKey().autoincrement().notNull(),
+  sessionId: smallint('session_id').notNull(),
+  driverId: smallint('driver_id').notNull(),
+  status: mysqlEnum('status', ['fin', 'dnf', 'dsq', 'ret']).notNull(),
+  position: smallint('position').notNull(),
+  points: smallint('points').notNull(),
+  penaltiesSeconds: smallint('penalties_seconds').notNull()
 });
-
-// export const roundsRelations = relations(rounds, ({ many }) => ({
-//   sessions: many(sessions)
-// }));
-
-// export const sessionsRelations = relations(sessions, ({ one, many }) => ({
-//   round: one(rounds, {
-//     fields: [sessions.roundId],
-//     references: [rounds.id]
-//   }),
-//   laps: many(laps),
-//   incidents: many(incidents),
-//   results: many(results)
-// }));
-
-// export const teamsRelations = relations(teams, ({ many }) => ({
-//   drivers: many(drivers)
-// }));
-
-// export const driversRelations = relations(drivers, ({ one, many }) => ({
-//   team: one(teams, {
-//     fields: [drivers.teamId],
-//     references: [teams.id]
-//   }),
-//   laps: many(laps),
-//   incidents: many(incidents),
-//   results: many(results)
-// }));
-
-// export const lapsRelations = relations(laps, ({ one }) => ({
-//   session: one(sessions, {
-//     fields: [laps.sessionId],
-//     references: [sessions.id]
-//   }),
-//   driver: one(drivers, {
-//     fields: [laps.driverId],
-//     references: [drivers.id]
-//   })
-// }));
-
-// export const incidentRelations = relations(incidents, ({ one }) => ({
-//   session: one(sessions, {
-//     fields: [incidents.sessionId],
-//     references: [sessions.id]
-//   }),
-//   driver: one(drivers, {
-//     fields: [incidents.driverId],
-//     references: [drivers.id]
-//   })
-// }));
-
-// export const resultsRelations = relations(results, ({ one }) => ({
-//   session: one(sessions, {
-//     fields: [results.sessionId],
-//     references: [sessions.id]
-//   }),
-//   driver: one(drivers, {
-//     fields: [results.driverId],
-//     references: [drivers.id]
-//   })
-// }));
