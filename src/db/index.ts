31320d055c1625bcf45f8e54926c853a2768373b
Maciej Pędzich <contact@maciejpedzi.ch>
2023-08-01 11:21:18 +0200

    Install and use sql-log-prettier to log queries

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,17 @@
+import type { Logger } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/mysql2';
+
 import mysql from 'mysql2/promise';
+import { prettify } from 'sql-log-prettifier';
+
+class PrettyLogger implements Logger {
+  logQuery(query: string, params: unknown[]): void {
+    if (import.meta.env.PROD) return;
+
+    console.log(`${prettify(query)}\nPARAMS: [${params}]\n`);
+  }
+}
 
 const connection = await mysql.createConnection(import.meta.env.DATABASE_URL);
 
-export const db = drizzle(connection, { logger: import.meta.env.DEV });
+export const db = drizzle(connection, { logger: new PrettyLogger() });
